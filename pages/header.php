c9d0ccbc68c3c359ce318f82ed0603aa86f7ca29
g-URL <randall.marie.burke@gmail.com>
2018-04-12 01:17:56 -0600

    header update

--- pages/header.php
+++ pages/header.php
@@ -18,7 +18,7 @@
         <a class="nav-link" href="login.php">Login</a>
         </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0" method="post">
+    <form class="form-inline my-2 my-lg-0" action="index.php" method="post">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="input">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Search</button>
     </form>
