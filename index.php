8c7188ca6721132a728de491defa7bb299535e45
Nate <ncarrigan@seekintoo.com>
2018-04-11 23:47:08 -0600

    updated index.php

--- index.php
+++ index.php
@@ -18,48 +18,22 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="./">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">          
-            <a class="nav-link" href="index.php">All Invasive Species <span class="sr-only">(current)</span></a>
-          </li>          
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-           <!-- <a class="nav-link disabled" href="#">Disabled</a> -->
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <!-- SEARCHING -->
-        <form class="form-inline my-2 my-lg-0" action="index.php" method="post">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="input">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    <?php include_once "header.php"; ?>
 
     <main role="main" class="container">
 
       <div class="starter-template">
       <?php
-      
+     
+      // ================================ CONNECTION =====================================
+      include_once "php/commons.php";
+
+      $conn = get_mysqli_localhost();
+      if ($conn->connect_error) {
+        die("Connection Error: (" . $conn->connect_errno . ")");
+      }
+
+
       // ================================ FUNCTIONS ======================================
       
       function randallrelevant($conn, $plant_id)
@@ -127,26 +101,6 @@
                 echo "<p>" ."<strong>Habitat: </strong>". $row["habitat"] . '</p>';                    
               }                        
       }
-      
-      // =============================== /FUNCTIONS ======================================     
-      
-
-
-
-
-      // =============================== CONNECTING ======================================
-       
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $database = "invdb";
-      // Create connection
-      $conn = mysqli_connect($servername, $username, $password, $database);
-      // Check connection
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-      echo "Connected successfully";
       if (isset($_GET['edit']))
       {
         $inv_sci_name = mysqli_real_escape_string($conn, $_GET['edit']);
